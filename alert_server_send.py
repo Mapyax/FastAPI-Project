2537c3bd0f1d155103dc701f3e887c6700e14883
Andrey <andrey.n.prokhorov@rt.ru>
2026-07-20 00:18:03 +0700

    Sending alerts to TG

--- alert_server_send.py
+++ alert_server_send.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import aio_pika
+from aiogram import Bot
 
 RABBITMQ_URL = "amqp://default:default@iot_rabbitmq:5672/"
 EXCHANGE_NAME = "metrics_exchange"
@@ -11,6 +12,10 @@ RAM_THRESHOLD_PERCENT = 30
 BATTERY_THRESHOLD_PERCENT = 30
 DISK_THRESHOLD_PERCENT = 15
 
+BOT_TOKEN = "" # TODO CONFIG FILE
+CHAT_ID = # TODO CONFIG FILE
+
+bot = Bot(token=BOT_TOKEN)
 
 async def process_message(message: aio_pika.abc.AbstractIncomingMessage):
     async with message.process():
@@ -27,17 +32,36 @@ async def process_message(message: aio_pika.abc.AbstractIncomingMessage):
             print(f"\n{ts}")
 
             if cpu > CPU_THRESHOLD_PERCENT:
-                print(f"Warning: High CPU Usage! Current: {cpu}%")
-                # TODO: tg_bot.send_message(...)
+                alert_text = (
+                    f"Warning: High CPU Usage! Current: {cpu}%\n"
+                    f"Time(Moscow): `{ts}`\n"
+                )
+                await bot.send_message(chat_id=CHAT_ID, text=alert_text, parse_mode="Markdown")
+                print("Sent CPU alert to TG")
 
             if ram > RAM_THRESHOLD_PERCENT:
-                print(f"Critical: Running out of RAM! Current: {ram}%")
+                alert_text = (
+                    f"Critical: Running out of RAM! Current: {ram}%\n"
+                    f"Time(Moscow): `{ts}`\n"
+                )
+                await bot.send_message(chat_id=CHAT_ID, text=alert_text, parse_mode="Markdown")
+                print("Sent RAM alert to TG")
 
             if battery != -1 and battery < BATTERY_THRESHOLD_PERCENT:
-                print(f"Critical: Almost out of battery! Current: {battery}%")
+                alert_text = (
+                    f"Critical: Almost out of battery! Current: {battery}%\n"
+                    f"Time(Moscow): `{ts}`\n"
+                )
+                await bot.send_message(chat_id=CHAT_ID, text=alert_text, parse_mode="Markdown")
+                print("Sent battery alert to TG")
 
             if disk_percent < DISK_THRESHOLD_PERCENT:
-                print(f"Warning: Running out of space! Current: {disk_free} GB")
+                alert_text = (
+                    f"Warning: Running out of space! Current: {disk_free} GB\n"
+                    f"Time(Moscow): `{ts}`\n"
+                )
+                await bot.send_message(chat_id=CHAT_ID, text=alert_text, parse_mode="Markdown")
+                print("Sent disk space alert to TG")
 
         except Exception as e:
             print(f"Error with the message: {e}")
@@ -63,16 +87,14 @@ async def main():
     )
 
     queue = await channel.declare_queue(name=QUEUE_NAME, durable=True)
-
     await queue.bind(exchange=exchange, routing_key="")
-
     print(f"=== Queue '{QUEUE_NAME}' Succesfully binded to the exchange. Waiting for the metrics... ===")
-
     await queue.consume(process_message)
 
     try:
         await asyncio.Future()
     finally:
+        await bot.session.close()
         await connection.close()
 
 
